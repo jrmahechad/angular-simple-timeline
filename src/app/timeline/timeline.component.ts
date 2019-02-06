7eb0af576194a51610d508649a9797346a87e158
Julian Mahecha <jmahecha@belatrixsf.com>
2019-02-06 14:23:14 -0500

    simple timeline using flexbox an angular

---
+++ src/app/timeline/timeline.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timeline',
+  templateUrl: './timeline.component.html',
+  styleUrls: ['./timeline.component.scss']
+})
+export class TimelineComponent implements OnInit {
+
+  public items = [
+    {
+      text: 'First Year',
+      active: true
+    },
+    {
+      text: 'Second Year',
+      active: true,
+      flipped: true
+    },
+    {
+      text: 'Third Year',
+    },
+    {
+      text: 'Fourth Year',
+      flipped: true
+    },
+    {
+      text: 'Fifth Year',
+    }
+
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  checkPrevItemActive(index) {
+    const prevIndex = index - 1;
+    return prevIndex > -1 ? this.items[prevIndex].active : false;
+  }
+
+  isLastItem(index) {
+    return index === this.items.length - 1 ;
+  }
+
+  isFirstItem(index) {
+    return index === 0;
+  }
+
+}
